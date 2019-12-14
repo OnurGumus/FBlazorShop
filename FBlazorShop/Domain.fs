687f6f08c20944606388974e72bdf84e81b9ade9
Onur Gumus <onur.gumus@emaratech.ae>
2019-12-14 23:42:48 +0400

    more saga refactoring

--- FBlazorShop/Domain.fs
+++ FBlazorShop/Domain.fs
@@ -63,11 +63,15 @@ module Common =
             ((event |> box), originator)
                 |> CheckSagas |> Command
 
-        let toSendMessage (event, originator) =
-            Send(SagaStarterPath, (event, originator) |> toCheckSagas)
+        let toSendMessage  mediator (originator : IActorRef<_>) event  =
+            let message = Send(SagaStarterPath, (event, untyped originator) |> toCheckSagas)
+            (mediator <? (message |> box)) |> Async.RunSynchronously |> function
+            | SagaCheckDone -> ()
 
-        let publishEvent (sender : IActorRef<_>) (self : IActorRef<_>) (mediator:IActorRef<_>) event=
 
+        let publishEvent (mailbox : Eventsourced<_>) (mediator:IActorRef<_>) event=
+            let sender = mailbox.Sender()
+            let self = mailbox.Self
             if sender.Path.Name |> isSaga then
                 let originatorName = sender.Path.Name |> toOriginatorName
                 if originatorName <> self.Path.Name then
@@ -75,6 +79,13 @@ module Common =
             else
                 sender <! event
             mediator <! box (Publish(self.Path.Name, event))
+        let cont (mediator : IActorRef<_>) =
+            mediator <! box (Send(SagaStarterPath, Continue |> Command))
+
+        let subscriber (mediator : IActorRef<_>) (mailbox : Eventsourced<_>) =
+            let originatorName = mailbox.Self.Path.Name |> toOriginatorName
+            mediator <! box (Subscribe(originatorName, untyped mailbox.Self))
+
 
 module Order =
 
@@ -98,20 +109,17 @@ module Order =
                     return! o |> Some |> set
 
                 | Command{Command = PlaceOrder o; CorrelationId = ci}, None ->
-                    let event = o |> OrderPlaced |> Event.toEvent ci |> Event
-                    let res =
-                        mediator <? ((event, untyped mailbox.Self ) |> SagaStarter.toSendMessage |> box)
-                        |> Async.RunSynchronously
-                    match res with
-                        | SagaStarter.SagaCheckDone ->
-                            return! event |> Persist
+                    //call saga starter and wait till it responds
+                    let event = o |> OrderPlaced |> Event.toEvent ci
+                    SagaStarter.toSendMessage mediator mailbox.Self event
+                    return! event |> Event |> Persist
 
                 | Command {Command = PlaceOrder o; CorrelationId = ci}, Some _ ->
+                    //An order can be placed once only
                     mailbox.Sender() <! (OrderRejected(o,"duplicate") |> Event.toEvent ci |> Event)
 
                 | Persisted mailbox (Event({Event = OrderPlaced o } as e)), _ ->
-                    let sender = mailbox.Sender()
-                    SagaStarter.publishEvent sender mailbox.Self mediator e
+                    SagaStarter.publishEvent mailbox mediator e
                     return! o |> Some |> set
                 | _ -> invalidOp "not supported"
             }
@@ -135,25 +143,43 @@ module OrderSaga =
         with interface IDefaultTag
 
     let actorProp (mediator : IActorRef<_>)(mailbox : Eventsourced<obj>)=
-        let originatorName =  mailbox.Self.Path.Name |> SagaStarter.toOriginatorName
-        let rec set state =
+        let originatorName = mailbox.Self.Path.Name |> SagaStarter.toOriginatorName
+        let rec set (state : State) =
             actor {
                     let! msg = mailbox.Receive()
                     match box(msg), state with
-                    | :? SubscribeAck as s, _ when  s.Subscribe.Topic = originatorName ->
-                        mediator <! box (Send(SagaStarter.SagaStarterPath, SagaStarter.Continue |> SagaStarter.Command))
+                    | :? SubscribeAck as s, _ when s.Subscribe.Topic = originatorName ->
+                        // notify saga starter about the subscription completed
+                        SagaStarter.cont mediator
                         return! set state
+
                     | PersistentLifecycleEvent ReplaySucceed ,_->
-                        mediator <! box (Subscribe(originatorName, untyped mailbox.Self))
+                        SagaStarter.subscriber mediator mailbox
+                        //  take recovery action for the current state
                         return! set state
+
                     | Recovering mailbox (:? Event as e), _ ->
+                        //replay the recovery
                         match e with
-                        | StateChanged s -> return! set (box(s))
-                    | Persisted mailbox e, _-> return! set e
-                    | :? Event as e, _  ->
-                        match e with
-                        | StateChanged state ->
-                           return! Persist(StateChanged (state)|>box)
+                        | StateChanged s -> return! set s
+
+                    | Persisted mailbox msg, _->
+                        match msg with
+                        | :? Event as e ->
+                            match e with
+                            | StateChanged state ->
+                                //take entry actions of new state
+                                return! set state
+
+                        | _ -> return! set state
+                    | :? Common.Event<Order.Event> as orderEvent, _ ->
+                        match orderEvent with
+                        | {Event = Order.OrderPlaced o } ->
+                          // decide new state
+                          return! Persist(StateChanged (state)|>box)
+
+                        | _ -> return! set state
+
                     | _ -> return! set state
             }
         set Started
@@ -181,6 +207,7 @@ module SagaStarter =
             actor {
                 match! mailbox.Receive() with
                 | Command (Continue) ->
+                    //check if all sagas are started. if so issue SagaCheckDone to originator else keep wait
                     let sender = untyped <| mailbox.Sender()
                     let originName = sender.Path.Name |> SagaStarter.toOriginatorName
                     let originator,subscribers = state.[originName]
@@ -196,8 +223,12 @@ module SagaStarter =
 
                 | Command(CheckSagas (o, originator)) ->
                     match o with
-                    | :? Order.Message ->
-                        return! set <| startSaga originator OrderSaga.factory
+                    | :? Common.Event<Order.Event> as e ->
+                        match e with
+                        | {Event = Order.OrderPlaced _  }  ->
+                            //start saga
+                            return! set <| startSaga originator OrderSaga.factory
+                        | _ -> mailbox.Sender() <! SagaCheckDone
 
                     | _ ->  mailbox.Sender() <! SagaCheckDone
                     return set state
@@ -206,13 +237,16 @@ module SagaStarter =
             }
         set Map.empty
 
-let init () =
-    let sagaStarter =
-        spawn Actor.system
-            <| SagaStarter.SagaStarterName
-            <| props SagaStarter.actorProp
+    let init =
+        let sagaStarter =
+            spawn Actor.system
+                <| SagaStarter.SagaStarterName
+                <| props actorProp
 
-    typed Actor.mediator <! (sagaStarter |> untyped |> Put)
+        typed Actor.mediator <! (sagaStarter |> untyped |> Put)
+
+let init () =
+    SagaStarter.init
     Order.init |> ignore
     OrderSaga.init |> ignore
     System.Threading.Thread.Sleep(1000)
