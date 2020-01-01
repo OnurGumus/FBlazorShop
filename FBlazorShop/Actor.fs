acffe2ddf6089db3b3cfa69c1fa5b04f67644f4a
Onur Gumus <onur.gumus@emaratech.ae>
2020-01-01 15:19:59 +0400

    use dotnetty

--- FBlazorShop/Actor.fs
+++ FBlazorShop/Actor.fs
@@ -60,7 +60,7 @@ let configWithPort port =
                 }
             }
             remote {
-                helios.tcp {
+                dot-netty.tcp {
                     public-hostname = "localhost"
                     hostname = "localhost"
                     port = """ + port.ToString() + """
